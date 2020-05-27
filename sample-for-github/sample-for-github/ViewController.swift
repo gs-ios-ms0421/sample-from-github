2ea3fdd7a4e847e7eca6840e2d72926ec4156c8a
motonao-suzue <2017.onlineuse@gmail.com>
2020-05-28 03:47:12 +0900

    first commit.

---
+++ sample-for-github/sample-for-github/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  sample-for-github
+//
+//  Created by 鈴江元尚 on 2020/05/28.
+//  Copyright © 2020 鈴江元尚. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("sample1")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
